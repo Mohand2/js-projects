fcd950efe8bdee05fb187217ea7592c8dd3e3f78
Mohand2 <alloun.mohand@gmail.com>
2022-07-22 14:37:03 +0100

    timer

--- guess_my_number/script.js
+++ guess_my_number/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+
+
+
+
+
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.querySelector('.message').textContent = 'correct';
+
+
+async function changeValue() {
+ 
+    for (let i = 0; i < 11; i++) {
+        document.querySelector('.guess').value = i;
+        if (i === 10) document.querySelector('.message').textContent = "Game Over !"
+  await sleep(1000);
+}
+
+}
+
+
+changeValue();
+
