213e95ce705d78ecee519db1a75f5825f3bc491d
Mohand2 <alloun.mohand@gmail.com>
2022-07-24 21:43:12 +0100

    added Roll dice feature

--- pig game/script.js
+++ pig game/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const diceImgs = [
+  'dice-1.png',
+  'dice-2.png',
+  'dice-3.png',
+  'dice-4.png',
+  'dice-5.png',
+  'dice-6.png',
+];
+
+const rollDiceBtn = document.querySelector('.btn--roll');
+const imgElement = document.querySelector('.dice');
+const randomDiceIndex = () => Math.floor(Math.random() * diceImgs.length);
+const changeDiceImg = () => (imgElement.src = diceImgs[randomDiceIndex()]);
+
+rollDiceBtn.addEventListener('click', changeDiceImg);
