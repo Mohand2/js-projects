540f7f8f7f8ab60d931aeb882750cd00cce88ac6
Mohand2 <alloun.mohand@gmail.com>
2022-07-24 11:28:43 +0100

    modal example

--- modal window/script.js
+++ modal window/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const showModalBtns = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < showModalBtns.length; i++) {
+  showModalBtns[i].addEventListener('click', function () {
+    console.log('Showing modal');
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+  });
+}
+
+closeModalBtn.addEventListener('click', function () {
+  modal.style.display = 'none';
+  overlay.style.display = 'none';
+});
+
+overlay.addEventListener('click', function () {
+  modal.style.display = 'none';
+  overlay.style.display = 'none';
+});
