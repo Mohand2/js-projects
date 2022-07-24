b12475af50cd732d0197ffd02f36d3315c32478b
Mohand2 <alloun.mohand@gmail.com>
2022-07-24 11:37:36 +0100

    removed print fn

--- modal window/script.js
+++ modal window/script.js
@@ -7,7 +7,6 @@ const overlay = document.querySelector('.overlay');
 
 for (let i = 0; i < showModalBtns.length; i++) {
   showModalBtns[i].addEventListener('click', function () {
-    console.log('Showing modal');
     modal.style.display = 'block';
     overlay.style.display = 'block';
   });
