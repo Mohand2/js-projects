b99f04c3c3a1ddb4053c7aef672564d19409ca87
Mohand2 <alloun.mohand@gmail.com>
2022-07-24 22:26:33 +0100

    added Roll dice function

--- pig game/script.js
+++ pig game/script.js
@@ -9,9 +9,53 @@ const diceImgs = [
   'dice-6.png',
 ];
 
+let diceValue;
+let currentScore = 0;
 const rollDiceBtn = document.querySelector('.btn--roll');
 const imgElement = document.querySelector('.dice');
-const randomDiceIndex = () => Math.floor(Math.random() * diceImgs.length);
-const changeDiceImg = () => (imgElement.src = diceImgs[randomDiceIndex()]);
+const currentScoreOfPlayer1 = document.querySelector('#current--0');
+const currentScoreOfPlayer2 = document.querySelector('#current--1');
+const Player1 = document.querySelector('.player--0');
 
-rollDiceBtn.addEventListener('click', changeDiceImg);
+const randomDiceIndex = () => Math.floor(Math.random() * diceImgs.length);
+const setDiceValue = index => {
+  switch (index) {
+    case 0:
+      diceValue = index + 1;
+      break;
+    case 1:
+      diceValue = index + 1;
+      break;
+    case 2:
+      diceValue = index + 1;
+      break;
+    case 3:
+      diceValue = index + 1;
+      break;
+    case 4:
+      diceValue = index + 1;
+      break;
+    case 5:
+      diceValue = index + 1;
+      break;
+
+    default:
+      break;
+  }
+};
+
+const changeDiceImg = () => {
+  let index = randomDiceIndex();
+  imgElement.src = diceImgs[index];
+  setDiceValue(index);
+};
+
+const rollDiceFunction = () => {
+  changeDiceImg();
+  currentScore += diceValue;
+  Player1.classList.contains('player--active')
+    ? (currentScoreOfPlayer1.textContent = currentScore)
+    : (currentScoreOfPlayer2.textContent = currentScore);
+};
+
+rollDiceBtn.addEventListener('click', rollDiceFunction);
