81971ea86de66b34f37ec0875fc1811b6f6154de
Mohand2 <alloun.mohand@gmail.com>
2022-07-24 12:08:49 +0100

    final version

--- modal window/script.js
+++ modal window/script.js
@@ -5,21 +5,18 @@ const modal = document.querySelector('.modal');
 const closeModalBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
-for (let i = 0; i < showModalBtns.length; i++) {
-  showModalBtns[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-
-    // Another solution
-    // modal.style.display = 'block';
-    // overlay.style.display = 'block';
-  });
-}
-
 const colseModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < showModalBtns.length; i++) {
+  showModalBtns[i].addEventListener('click', openModal);
+}
 
 closeModalBtn.addEventListener('click', colseModal);
 overlay.addEventListener('click', colseModal);
