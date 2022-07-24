34fe30fb751c8425353c8cec88833005179b5517
Mohand2 <alloun.mohand@gmail.com>
2022-07-24 11:44:57 +0100

    optimisation

--- modal window/script.js
+++ modal window/script.js
@@ -5,6 +5,8 @@ const modal = document.querySelector('.modal');
 const closeModalBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
+const switcher = [closeModalBtn, overlay];
+
 for (let i = 0; i < showModalBtns.length; i++) {
   showModalBtns[i].addEventListener('click', function () {
     modal.style.display = 'block';
@@ -12,12 +14,9 @@ for (let i = 0; i < showModalBtns.length; i++) {
   });
 }
 
-closeModalBtn.addEventListener('click', function () {
-  modal.style.display = 'none';
-  overlay.style.display = 'none';
-});
-
-overlay.addEventListener('click', function () {
-  modal.style.display = 'none';
-  overlay.style.display = 'none';
-});
+for (let i = 0; i < switcher.length; i++) {
+  switcher[i].addEventListener('click', function () {
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+  });
+}
